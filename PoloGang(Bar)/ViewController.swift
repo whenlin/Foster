2685acc6e23c8f4b9d73467254b18263cb2f0ee2
WHenlin45 <whenlin@uwo.ca>
2018-06-09 15:05:21 -0400

    Initial Commit

---
+++ PoloGang(Bar)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoloGang(Bar)
+//
+//  Created by WHenlin on 2018-06-09.
+//  Copyright © 2018 WHenlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
